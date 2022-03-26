0898e24407041dd510c5a38e0c2087496b0e8116
minh <minhchinh54@gmail.com>
2022-03-26 13:28:03 +0700

    Fix screen mobile

---
+++ assets/img/icons/index.js
@@ -0,0 +1,71 @@
+import appleIcon from './appleIcon.png';
+import googleIcon from './googleIcon.png';
+import facebookIcon from './facebookIcon.png';
+import heart from './heart.png';
+import home from './home.png';
+import location from './location.png';
+import study from './study.png';
+import suitcase from './suitcase.png';
+import info from './info.png';
+import back from './back.png';
+import sent from './sent.png';
+import close from './close.png';
+import photoPlus from './photoPlus.png';
+import discover from './discover.png';
+import discoverActive from './discoverActive.png';
+import like from './like.png';
+import likeActive from './likeActive.png';
+import talk from './talk.png';
+import talkActive from './talkActive.png';
+import user from './user.png';
+import userActive from './userActive.png';
+import logo from './logo.png';
+import settings from './settings.png';
+import draw from './draw.png';
+import edu from './edu.png';
+import infor from './infor.png';
+import intro from './intro.png';
+import style from './style.png';
+import photo from './photo.png';
+import next from './next.png';
+import search from './search.png';
+
+import locationRequest from './locationRequest.png';
+
+const ICONS = {
+  search,
+  locationRequest,
+  next,
+  photo,
+  edu,
+  infor,
+  intro,
+  style,
+  settings,
+  draw,
+  logo,
+  discover,
+  discoverActive,
+  like,
+  likeActive,
+  talk,
+  talkActive,
+  user,
+  userActive,
+  appleIcon,
+  googleIcon,
+  facebookIcon,
+  heart,
+  home,
+  location,
+  study,
+  suitcase,
+  info,
+  back,
+  sent,
+  close,
+  photoPlus,
+  next,
+};
+
+export default ICONS;
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Main là file index
+
